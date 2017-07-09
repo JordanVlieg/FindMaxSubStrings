b43fdb6402db671972ac1f58b7aceb98b579adfb
Jordan Vlieg <jordanvlieg@gmail.com>
2017-07-08 17:08:14 -0700

    Refactoring

--- FrozenMountainQuiz/Program.cs
+++ FrozenMountainQuiz/Program.cs
@@ -11,28 +11,28 @@ namespace FrozenMountainQuiz
     {
         static String FindMaxSubStrings(int len, String searchString, out int maxOccurrences)
         {
-            if (len <= 0 || len > searchString.Length || searchString.Length <= 0)
+            if (len <= 0 || searchString.Length < len || searchString.Length <= 0)
             {
                 // Originally I had these seperate and throwing exceptions, but for this scale I think this is adequate.
                 maxOccurrences = 0;
                 return "BAD PARAMETERS";
             }
-            Dictionary<string, int> tempMap = new Dictionary<string, int>();
+            Dictionary<string, int> ssMap = new Dictionary<string, int>();
 
             for (var offset = 0; offset + len < searchString.Length; offset++)
             {
-                var sub = searchString.Substring(offset, len);
-                if (!tempMap.ContainsKey(sub))
+                var subStr = searchString.Substring(offset, len);
+                if (!ssMap.ContainsKey(subStr))
                 {
-                    tempMap.Add(sub, 1);
+                    ssMap.Add(subStr, 1);
                 }
                 else
                 {
-                    tempMap[sub]++;
+                    ssMap[subStr]++;
                 }
             }
-            maxOccurrences = tempMap.Values.Max();
-            foreach (var entry in tempMap)
+            maxOccurrences = ssMap.Values.Max();
+            foreach (var entry in ssMap)
             {
                 if (entry.Value == maxOccurrences)
                 {
@@ -45,7 +45,8 @@ namespace FrozenMountainQuiz
         static void Main(string[] args)
         {
             int occurrences = 0;
-            String theString = FindMaxSubStrings(3, "zfabcde224lkfabc51+crsdtab=", out occurrences);
+            String theString = FindMaxSubStrings(2, "2tg5iTPoViM6PM5Nit6HVYG2D8lhS80Bpo0cMyxTaeFW5zbblahobZuKERgro7FL6CCgeWa2M7" +
+                "9PDQqgNamCUZhrgIbu1lBbhGVRtEewtVblah05TLi6qYwPOLmUgQYaGNWwHPcoKblahergOcTu9WThk", out occurrences);
             if(occurrences > 0)
             {
                 Console.WriteLine("The substring '" + theString + "' occurs " + occurrences + " times");
